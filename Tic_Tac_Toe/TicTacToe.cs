66d9c07241e1f8563a792fedfcab72d9fd5bbc2e
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-17 12:37:49 +0530

    GAME TIC TAC TOE

---
+++ Tic_Tac_Toe/TicTacToe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class Tic_Tac_ToeGame
+    {
+        char[] arr = new char[10];
+
+        public void Board()
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = ' ';
+            }
+        }
+    }
+}
+
+
+        
